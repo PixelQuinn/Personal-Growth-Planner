c1e146ed028643a16ab843fb84dd50c5757251e4
Quinn Reams <quinn.reams@icloud.com>
2025-01-09 19:21:47 -0500

    feat: params defined for recipe-fetch

--- scripts/recipe-fetch.py
+++ scripts/recipe-fetch.py
@@ -6,3 +6,9 @@ api_key = "YOUR_API_KEY_HERE"
 base_url = "https://api.spoonacular.com/recipes/complexSearch"
 
 # Define query params
+params = {
+    "apiKey": api_key,
+    "number": 10, # <-- Number of recipes to fetch
+    "type": "main course", # <-- Filter by meal type
+    "maxCalories": 600
+}
