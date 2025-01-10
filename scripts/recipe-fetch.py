c1650fbb35ae8459a927dc351d8981837b373d68
Quinn Reams <quinn.reams@icloud.com>
2025-01-09 20:34:02 -0500

    script: script tested, solid work!

--- scripts/recipe-fetch.py
+++ scripts/recipe-fetch.py
@@ -2,19 +2,21 @@ import requests
 import pandas as pd
 
 # Set up API key
-api_key = "YOUR_API_KEY_HERE"
+api_key = "YOUR_API_HERE"
 base_url = "https://api.spoonacular.com/recipes/complexSearch"
 
 # Take user input
 print("Welcome to the Recipe Fetcher!")
 meal_type = input("Enter the meal type (e.g. breakfast, lunch, dinner): ").strip().lower()
 recipe_count = int(input("How many recipes would you like to fetch?: "))
+calorie_limit = int(input("What is the amount of calories you'd like to limit yourself to?: "))
 
 # Define query params
 params = {
     "apiKey": api_key,
     "number": recipe_count, # <-- Number of recipes to fetch
     "type": meal_type, # <-- Filter by meal type
+    "maxCalories": calorie_limit # <-- Limit calories for meals
 }
 
 # API request
